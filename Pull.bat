b2cf6430bcac512f64d2177ad7655ecb7ae09e11
Vladimir Lin <lin.vladimir@gmail.com>
2023-03-27 19:12:56 +0800

    Commit Website to GITHUB

---
+++ Pull.bat
@@ -0,0 +1,1 @@
+git pull https://Vladimir-Lin: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@github.com/Vladimir-Lin/vladimir-lin.github.io
---
+++ Push.bat
@@ -0,0 +1,2 @@
+git commit -a -m "Commit Website to GITHUB"
+git push
